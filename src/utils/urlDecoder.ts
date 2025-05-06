04a4996c28932b37250e82410950f638928ae87c
thomasmoulin <thomas.moulin@wttj.co>
2025-05-06 15:36:00 +0200

    add logs in URL decoder

--- src/utils/urlDecoder.ts
+++ src/utils/urlDecoder.ts
@@ -9,29 +9,62 @@ export function getCompanyDataFromUrl(): CompanyData {
       return getDefaultCompanyData();
     }
 
+    console.log("URL location:", window.location.href);
+
     // Try to get data from hash fragment first (format: #data=base64_encoded_json)
     let encodedData = null;
     
     if (window.location.hash) {
+      console.log("Hash detected:", window.location.hash.substring(0, 20) + "...");
       const hashMatch = window.location.hash.match(/^#data=(.+)$/);
       if (hashMatch && hashMatch[1]) {
         encodedData = hashMatch[1];
+        console.log("Found data in hash fragment, length:", encodedData.length);
+      } else {
+        console.log("Hash format doesn't match expected pattern");
       }
+    } else {
+      console.log("No hash fragment found in URL");
     }
     
     // If not found in hash, try query params as fallback
     if (!encodedData) {
+      console.log("Checking query parameters");
       const urlParams = new URLSearchParams(window.location.search);
       encodedData = urlParams.get('data');
+      if (encodedData) {
+        console.log("Found data in query params, length:", encodedData.length);
+      } else {
+        console.log("No data found in query parameters");
+      }
     }
 
     // If we have encoded data, decode and return it
     if (encodedData) {
-      const decodedData = Base64.decode(encodedData);
-      return JSON.parse(decodedData) as CompanyData;
+      try {
+        console.log("Attempting to decode Base64 data");
+        const decodedData = Base64.decode(encodedData);
+        console.log("Base64 decode successful, data length:", decodedData.length);
+        
+        try {
+          console.log("Attempting to parse JSON data");
+          const parsedData = JSON.parse(decodedData) as CompanyData;
+          console.log("JSON parse successful");
+          return parsedData;
+        } catch (jsonError) {
+          console.error("JSON parse error:", jsonError);
+          console.log("First 100 chars of decoded data:", decodedData.substring(0, 100));
+          throw jsonError;
+        }
+      } catch (decodeError) {
+        console.error("Base64 decode error:", decodeError);
+        console.log("First 100 chars of encoded data:", encodedData.substring(0, 100));
+        throw decodeError;
+      }
     }
     
     // If no data found in URL, return default data
+    console.log("No data found in URL, using default data");
     return getDefaultCompanyData();
   } catch (error) {
     console.error("Error parsing company data from URL:", error);
