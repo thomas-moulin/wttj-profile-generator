ff8458ad4366802525e13bc79a58a264559d54b4
thomasmoulin <thomas.moulin@wttj.co>
2025-05-07 18:11:36 +0200

    try auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "wttjprofilegenerator2a599885": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s34b3c60ce": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
